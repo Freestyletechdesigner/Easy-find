6f5418fb97046138648bc197fd0c5ca70fb7ad84
Freestyledesigner <chiazagomfreedom@icloud.com>
2026-07-30 22:19:41 +0100

    fix

--- app.js
+++ app.js
@@ -512,8 +512,6 @@ app.use((err, req, res, next) => {
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, '404.html'));
 });
-console.log('Initial startup: triggering background scraper pipeline...');
-runPipeline();
 //  Schedule to run every hour (at the start of every hour)
 cron.schedule('0 * * * *', () => {
   console.log('Running automated background scraper pipeline...');
